0da6de713bd4e1f3d5c5d07758981fe1fb27b7b0
himal <himal.rg@gmail.com>
2024-12-18 10:24:56 +0000

    updated query to find all roles for user based on provided id instead of id 1

--- src/main/java/com/prakass/aps/dao/UserAccountRepository.java
+++ src/main/java/com/prakass/aps/dao/UserAccountRepository.java
@@ -1,10 +1,11 @@
 package com.prakass.aps.dao;
 
 import com.prakass.aps.entities.user_account.UserAccountEntity;
-import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 public interface UserAccountRepository extends JpaRepository<UserAccountEntity, Long> {
     UserAccountEntity findFirstByEmail(String email);
@@ -14,7 +15,9 @@ public interface UserAccountRepository extends JpaRepository<UserAccountEntity, 
             UNION
             SELECT gr.role FROM group_role gr
             JOIN user_group ug ON gr.group_id = ug.group_id
-            WHERE ug.user_account_id = 1
+            WHERE ug.user_account_id = :id
             """, nativeQuery = true)
     Set<String> findAllRolesByUserId(@Param("id") Long id);
+
+    boolean existsByEmail(String email);
 }
