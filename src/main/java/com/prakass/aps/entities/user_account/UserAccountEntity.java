23b2901633cf31a0a810da4c4da792f6baa5a32d
Sudarshan <subarnabasnt546@gmail.com>
2025-04-16 13:31:43 +0545

    Fixing line intendent

--- src/main/java/com/prakass/aps/entities/user_account/UserAccountEntity.java
+++ src/main/java/com/prakass/aps/entities/user_account/UserAccountEntity.java
@@ -1,9 +1,11 @@
 package com.prakass.aps.entities.user_account;
 
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+
 import lombok.*;
 
 @Entity
@@ -14,33 +16,33 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class UserAccountEntity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String guid;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String passwordHash;
+    private String countryCode;
+    private String phoneNumber;
+    private boolean emailVerified;
+    private boolean phoneVerified;
+    private String accountStatus;
+    private boolean mfaEnabled;
+    private int failedLoginAttempts;
+    private LocalDateTime lastLogin;
+    private LocalDateTime lastPasswordChanged;
+    private String passwordResetToken;
+    private LocalDateTime passwordResetTokenGenerated;
+    private LocalDateTime accountLockedUntil;
+    private String securityQuestion;
+    private String securityQuestionHash;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 
-  private String guid;
-  private String firstName;
-  private String lastName;
-  private String email;
-  private String username;
-  private String passwordHash;
-  private String countryCode;
-  private String phoneNumber;
-  private boolean emailVerified;
-  private boolean phoneVerified;
-  private String accountStatus;
-  private boolean mfaEnabled;
-  private int failedLoginAttempts;
-  private LocalDateTime lastLogin;
-  private LocalDateTime lastPasswordChanged;
-  private String passwordResetToken;
-  private LocalDateTime passwordResetTokenGenerated;
-  private LocalDateTime accountLockedUntil;
-  private String securityQuestion;
-  private String securityQuestionHash;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
-  private LocalDateTime deletedAt;
-
-  @Transient private Set<String> roles = new HashSet<>();
+    @Transient
+    private Set<String> roles = new HashSet<>();
 }
